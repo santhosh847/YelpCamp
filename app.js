ba9ce5ad89eff8399dae06f3d301bc644665392a
San <santhoshvijayakumar847@gmail.com>
2023-06-28 11:07:38 +0530

    added mongodb

--- app.js
+++ app.js
@@ -144,6 +144,8 @@ app.use((err, req, res, next) => {
 	res.status(statusCode).render("alert", { err });
 });
 
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
 	console.log("serving on port 3000");
 });
